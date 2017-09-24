7a946e74dd590913a90e7a541523530c324732fc
raghav <raghavjindal2003@gmail.com>
2017-09-24 19:19:40 +0530

    building rnn in keras

---
+++ Deep_Learning/keras/rnn_keras/intro.py
@@ -0,0 +1,7 @@
+import keras
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Embedding
+from keras.layers import LSTM
+from keras.datasets import imdb
+
